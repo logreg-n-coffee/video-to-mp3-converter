295761c9a260bda5ebdeed07d002734bf24e3cf2
Rui Hu <hurui141@gmail.com>
2023-02-09 17:01:51 -0700

    Bug Fix: mysql config typos on lines 11-15

--- auth/server.py
+++ auth/server.py
@@ -8,11 +8,11 @@ server = Flask(__name__)
 mysql = MySQL(server)
 
 # config
-server.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
-server.config['MYSQL_USER'] = os.environ.get('MYSQL_HOST')
-server.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_HOST')
-server.config['MYSQL_DB'] = os.environ.get('MYSQL_HOST')
-server.config['MYSQL_PORT'] = os.environ.get('MYSQL_HOST')
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
 
 
 # endpoints
